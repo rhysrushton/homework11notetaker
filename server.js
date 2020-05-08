f4702c1b74b0e94a13c45dc859c96db23e82221a
Rhys Rushton <rhys.rushton@gmail.com>
2020-05-08 16:34:10 +1000

    get db

--- server.js
+++ server.js
@@ -54,6 +54,16 @@ noteApp.post("/api/notes", function(req,res){
     
 })
 
+//Get
+noteApp.get("/api/notes", function(req, res){
+    fs.readFile("./db/db.json", "utf8", function(err, data){
+        if(err){
+            throw(err)
+        } return res.json(JSON.parse(data))
+    })
+
+}); 
+
 
 
 //Listen
